8c75dbeb6a5b8bf7e02ba123af49469e9c51fb77
ValeryATotskiy <vtotskiy@gmail.com>
2018-01-12 16:55:18 +0300

    First commit

---
+++ src/main/resources/public/js/surok.main.js
@@ -0,0 +1,8 @@
+var app = angular.module("surokAppMain", []);
+app.controller("AppCtrl", function($scope){
+    $scope.websites = [{
+        id: '1',
+        name:'ыввыа',
+        desc:'no'
+    }];
+});
