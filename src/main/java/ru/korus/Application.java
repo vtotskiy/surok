11e089038f459cb68340f20d3c77e8747da9c601
ValeryATotskiy <vtotskiy@gmail.com>
2018-07-27 09:14:13 +0300

    Работающее состояние с boot-spring-web

--- src/main/java/ru/korus/Application.java
+++ src/main/java/ru/korus/Application.java
@@ -1,7 +1,10 @@
 package ru.korus;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
